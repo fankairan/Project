2b047b368d13871b97152363ea89baf9301ff8c6
fankairan <13718584863m@sina.cn>
2018-12-10 20:59:29 +0800

    视频模块

--- HXSY/product/js/index.js
+++ HXSY/product/js/index.js
@@ -1,4 +1,26 @@
 
+//-----box1 底部按钮切换大图-----
+$('#main_body>.main_box_1>.box_bott_part li a').on('click',function(e){
+        e.preventDefault();
+        var $a=$(this);
+        var $as=$('#main_body>.main_box_1>.box_bott_part li a');
+        var i=parseInt($as.index($a));
+        $a.css('background-position',`-${i*159}px -100px`).parent().siblings().children().css('background-position','').removeClass('act');
+
+        $(`#main_body>.main_box_1>.box_top_part ul>li:nth-child(${i+1})`).removeClass('none').siblings().addClass('none');
+});
+
+//--------box2 头部a切换--------
+ $('#main_box_2>.box_top_2 li a').on('click',function(e){
+    e.preventDefault();
+    var $a=$(this);
+    var $as=$('#main_box_2>.box_top_2 li a');
+    var i=parseInt($as.index($a)+1);
+    $a.addClass('act').parent().siblings().children().removeClass('act');
+    $(`#main_box_2>div:nth-child(${i+1})`).removeClass('none').siblings().addClass('none');
+    $('#main_box_2>div:first-child').removeClass('none');
+
+ });
 
 
 
@@ -44,10 +66,37 @@ $('#main_box_2>div:nth-child(2)>div:last-child>div>a').on('click',function(e){
     //--  获取对应视频ID  --
     var i=parseInt($as.index($a)+1);
     //---------------------------------
-     
+    $('#video_player').addClass('act_play');
+    $('#cover_up').removeClass('none');
+     //----  视频关闭按钮  ----
+       $('#video_player>.close').on('click',function(e){
+            e.preventDefault();
+            $('#video_player').removeClass('act_play')
+            $('#cover_up').addClass('none');
+        })
 
-
-
+     //--------动态获取 index视频路径----------3
+     var vid=i;
+     $.ajax({
+        url:'http://127.0.0.1:3000/video/indexVideo',
+        type:'get',
+        data:{vid},
+        dataType:'json',
+        success:function(res){
+            var data=res[0];
+            var vName=data.video_name;
+            var vSrc=data.video_src;
+            var html=`<div class='v_box'>
+                        <h2>${vName}</h2>
+                        <video src='${vSrc}' id='v_index' controls autoplay></video>
+                      </div>`;
+            $('div.v_box').remove();
+            $('#video_player').append(html);     
+        }
+     })
 })
 
-//--------动态获取 index视频路径----------
+
+
+
+
